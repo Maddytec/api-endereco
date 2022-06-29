4c6b3e2ae36aa84f4c562e39366952a3bc8c9288
MateusAlves01 <axlmxx@gmail.com>
2022-06-29 20:16:01 -0300

    Projeto inicializado #001

---
+++ src/main/java/br/com/maddytec/endereco/EnderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.maddytec.endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderecoApplication.class, args);
+	}
+
+}
