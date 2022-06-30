6f4647654c9fe665cca38bf7e8ede156417ef301
MateusAlves01 <axlmxx@gmail.com>
2022-06-29 22:30:39 -0300

    Adicionado get para consultar endereco por cep #002

---
+++ src/main/java/br/com/maddytec/endereco/client/ViaCepClient.java
@@ -0,0 +1,13 @@
+package br.com.maddytec.endereco.client;
+
+import br.com.maddytec.endereco.dto.EnderecoViaCepResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "viacep", url="https://viacep.com.br/ws")
+public interface ViaCepClient {
+     @GetMapping("/{cep}/json")
+     EnderecoViaCepResponseDTO getEndereco(@PathVariable String cep);
+
+}
